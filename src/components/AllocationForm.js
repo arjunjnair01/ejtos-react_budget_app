ae89e6b971db3a7d351488be380ab2ee35609edc
arjunjnair01 <arjunjnair01@gmail.com>
2024-05-07 15:17:57 -0400

    final project

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -58,7 +58,7 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                    <span style={{ marginLeft: '20px'}}> £</span>
                     <input
                         required='required'
                         type='number'
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,16 +1,33 @@
 
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
-
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const { expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const value = event.target.value;
+
+        // Check if the entered value is a valid number
+        if (!isNaN(value) && value !== '') {
+            // Check if the value is within the allowed range
+            if (parseInt(value) > 20000) {
+                alert('Budget cannot exceed £20,000.');
+            }
+            if (parseInt(value) < totalExpenses) {
+                alert('Budget cannot be lower than total spending.');
+            }
+            else {
+                setNewBudget(value);
+            }
+        }
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: £</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
